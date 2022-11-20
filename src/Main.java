3d007e2f6524bb21f24971aa7129d0b6894eb7c6
Евгений <Kolchak-zha@yandex.ru>
2022-11-21 02:35:43 +0400

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String example = "Hello, world";
+        int midpoint = example.length() / 2;
+        String firstHalf = example.substring(0, midpoint);
+        String secondHalf = example.substring(midpoint);
+        System.out.println(firstHalf);
+        System.out.println(secondHalf);
     }
 }
